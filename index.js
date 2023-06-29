c4f2014df1885fb439f8e72f69e9aeefebdd61d7
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-06-29 13:42:51 -0500

    working with .filter

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+const drivers2 = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching (array, name){
+    return array.filter(element => element.toLowerCase() === name.toLowerCase())
+  }
+
+function fuzzyMatch(array, letters){
+    return array.filter(element => element.slice(0,2).toLowerCase() === letters.toLowerCase())    
+}
+
+function matchName(array, name){
+    return array.filter(element => element.name === name)
+}
